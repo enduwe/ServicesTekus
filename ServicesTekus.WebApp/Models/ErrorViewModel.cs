7a0cf24340bb39b9ab98cecf0357185dd0943dd9
Etriana <Administrador@COBODES540.sistran.local>
2022-02-21 08:30:44 -0500

    Version Inicial 1.0

---
+++ ServicesTekus.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServicesTekus.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
